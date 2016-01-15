04572a57e0b8323549d94de3396bc93f5574420b
Kristen-Huber <kristen.huber.19@gmail.com>
2016-01-15 18:48:49 +0900

    chat

---
+++ chat.py
@@ -0,0 +1,15 @@
+""" 
+This is my second commit
+"""
+greeting="Hello"
+farewell="goodbye"
+minutes=0
+finishedTalking=False
+
+print(greeting)
+
+If finishedTalking=False:
+	minutes ++
+	If minutes==5:
+		finishedTalking=True
+print(farewell)
---
+++ hello_world.py
@@ -0,0 +1,6 @@
+"""
+This is my first commit on git
+"""
+
+greeting="Hello"
+print(greeting)
