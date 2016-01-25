90cd22704a6bb24bef6d22ead17fe950234b20ff
Kristen-Huber <kristen.huber.19@gmail.com>
2016-01-25 14:15:32 +0900

    prob.py

--- Prob/prob.py
+++ Prob/prob.py
@@ -8,8 +8,8 @@ Created on Sun Jan 24 17:00:09 2016
 
 import collections
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-count=0
 data=[1,2,3,4,2,6,4,2,1,5,4,3,2,5,2,2]
 
 c = collections.Counter(data)
@@ -23,15 +23,23 @@ def find_frequency(items):
         print("The frequency of number " + str(k) + " is " + str(float(v)/count_sum))
     
 def save_boxplot(items):
+    plt.figure()
     plt.boxplot(data)
     plt.savefig('boxplot.png')
     
 def save_histogram(items):
+    plt.figure()
     plt.hist(data,histtype='bar')
     plt.savefig('histogram.png')
+    
+def save_QQplot(items):
+    plt.figure()    
+    stats.probplot(data, dist="norm", plot= plt)
+    plt.savefig('QQplot.png')
     
 find_frequency(data)
 save_boxplot(data)
 save_histogram(data)
+save_QQplot(data)
 
         
